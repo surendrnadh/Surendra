23afdd382a2f9b056a28b156da06a19897ebf488
surendrnadh <156899810+surendrnadh@users.noreply.github.com>
2024-01-17 15:12:15 +0530

    Uploaded
    commit

---
+++ Reverse order.py
@@ -0,0 +1,15 @@
+''''
+# list reverse 1
+
+list=[1,2,3,4,5]
+print("list before reverse:",list)
+list.reverse()
+print("list after reverse:",list)
+
+'''
+'''
+# list of elements 2
+list=[1,2,3,4,5]
+print("using index no:",list[4])
+
+'''
